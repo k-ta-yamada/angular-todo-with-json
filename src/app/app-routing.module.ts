6535b556b095f8853a2c83a6f3fd440020a2308b
k-ta-yamada <key.luvless@gmail.com>
2019-01-21 07:48:56 +0900

    add todo/page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageComponent } from './todo/page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'todo', pathMatch: 'full' },
+  { path: '', component: PageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

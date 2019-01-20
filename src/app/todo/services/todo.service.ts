bfbad93a5c08f52c57f4821c204994dc4713ce4a
k-ta-yamada <key.luvless@gmail.com>
2019-01-21 07:55:26 +0900

    add TodoService

---
+++ src/app/todo/services/todo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  url = 'api/todos';
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.url);
+  }
+}

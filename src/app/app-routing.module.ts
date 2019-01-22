759a279207f6b140eaee00a550cb1ad09eada3eb
k-ta-yamada <key.luvless@gmail.com>
2019-01-22 21:13:26 +0900

    [upd] app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoPageComponent } from './pages/todo-page/todo-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'todo', pathMatch: 'full' },
+  { path: 'todo', component: TodoPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

4709c4ae55c3b412591f12b8e72a05ab5df9728f
k-ta-yamada <key.luvless@gmail.com>
2019-01-22 20:59:06 +0900

    [add] test

--- src/app/component/todo-input/todo-input.component.spec.ts
+++ src/app/component/todo-input/todo-input.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoInputComponent } from './todo-input.component';
 
-describe('TodoInputComponent', () => {
+xdescribe('TodoInputComponent', () => {
   let component: TodoInputComponent;
   let fixture: ComponentFixture<TodoInputComponent>;
 
--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,12 +1,61 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { Todo } from '../models/todo';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: TodoService;
+  let httpTestingController: HttpTestingController;
 
-  it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoService],
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(TodoService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  const result: Todo[] = [
+    { id: 1, task: 'aaa', done: false },
+    { id: 2, task: 'aaa', done: false },
+    { id: 3, task: 'aaa', done: false },
+  ];
+
+  it('#get', () => {
+    service.get()
+      .subscribe(
+        r => expect(r).toEqual(result),
+        fail
+      );
+
+    const req = httpTestingController.expectOne(service.url);
+    expect(req.request.method).toEqual('GET');
+    req.flush(result);
+  });
+
+  it('#add', () => {
+    const todo: Todo = { id: 1, task: 'add task', done: false };
+    service.add(todo).subscribe(
+      r => {
+        console.group('#add.subscribe');
+        console.log('ADDED!!!!');
+        console.log(r);
+        console.groupEnd();
+        expect(r).toEqual(todo);
+      },
+      fail
+    );
+
+    const req = httpTestingController.expectOne(service.url);
+    expect(req.request.method).toEqual('POST');
+    req.flush(todo);
   });
 });

ccaa04f483ba2f224b558af96db4940470e1fe77
k-ta-yamada <key.luvless@gmail.com>
2019-01-25 00:15:33 +0900

    [fix] services/todo.service.spec.ts

--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,12 +1,78 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TodoService } from './todo.service';
+import { Todo } from '../models/todo';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpTestingController: HttpTestingController;
+  let todoService: TodoService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    todoService = TestBed.get(TodoService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
-    expect(service).toBeTruthy();
+    expect(todoService).toBeTruthy();
+  });
+
+  it('#get', () => {
+    const todos: Todo[] = [];
+
+    todoService.get().subscribe(
+      result => { expect(result).toBe(todos); },
+      fail
+    );
+
+    const req = httpTestingController.expectOne(todoService.url);
+    expect(req.request.method).toBe('GET');
+    req.flush(todos);
+  });
+
+  it('#add', () => {
+    const todo: Todo = { id: 42, task: 'new todo', done: false };
+
+    todoService.add(todo).subscribe(
+      result => { expect(result).toBe(todo); },
+      fail
+    );
+
+    const req = httpTestingController.expectOne(todoService.url);
+    expect(req.request.method).toBe('POST');
+    req.flush(todo);
+  });
+
+  it('#upd', () => {
+    const todo: Todo = { id: 42, task: 'new todo', done: false };
+
+    todoService.upd(todo).subscribe(
+      result => { expect(result).toBe(todo); },
+      fail
+    );
+
+    const req = httpTestingController.expectOne(`${todoService.url}/${todo.id}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(todo);
+  });
+
+  it('#del', () => {
+    const todo: Todo = { id: 42, task: 'new todo', done: false };
+    const body = {};
+
+    todoService.del(todo).subscribe(
+      result => { expect(result).toBe(body); },
+      fail
+    );
+
+    const req = httpTestingController.expectOne(`${todoService.url}/${todo.id}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(body);
   });
 });

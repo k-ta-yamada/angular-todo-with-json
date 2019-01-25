1e03e4ec49761a02df2ba4013015a7c970969d98
k-ta-yamada <key.luvless@gmail.com>
2019-01-25 18:14:14 +0900

    [fix] all specs

--- src/app/component/todo-list/todo-list.component.spec.ts
+++ src/app/component/todo-list/todo-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
@@ -8,7 +9,8 @@ describe('TodoListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
+      declarations: [TodoListComponent],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));

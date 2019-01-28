c729e7512c77aca343c63bfdb6e92e2884c81a13
k-ta-yamada <key.luvless@gmail.com>
2019-01-28 18:29:27 +0900

    [wip] ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +20,8 @@ import { TodoDetailComponent } from './component/todo-detail/todo-detail.compone
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/todo-detail/todo-detail.component.ts
+++ src/app/component/todo-detail/todo-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-detail',
@@ -8,22 +9,38 @@ import { Todo } from 'src/app/models/todo';
 })
 export class TodoDetailComponent implements OnInit {
 
-  todo: Todo = new Todo;
+  todo = new Todo;
 
-  keyDownCode: number;
+  todoForm = this.fb.group({
+    task: [this.todo.task, [Validators.required]],
+    done: [this.todo.done, []],
+    dummy: this.fb.group({
+      dummy1: ['', []],
+      dummy2: ['', []],
+    }),
+    dummyArray: this.fb.array([
+      this.fb.control('array1'),
+      this.fb.control('array2'),
+    ]),
+  });
 
   @Output() clickAdd = new EventEmitter<Todo>();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
-  add(e: KeyboardEvent) {
-    if (this.keyDownCode !== 13) { return; }
+  onSubmit() {
+    // this.todo.task = this.todoForm.get('task').value;
+    // this.todo.done = this.todoForm.get('done').value;
+
+    this.todo = new Todo(this.todoForm.value);
+    console.log(this.todo);
 
     this.clickAdd.emit(this.todo);
-    this.todo = new Todo;
+
+    this.todoForm.patchValue(new Todo);
+    // this.todoForm.reset();
   }
 
 }

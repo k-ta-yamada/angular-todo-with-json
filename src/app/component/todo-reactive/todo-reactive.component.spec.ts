96b1a53d3aa69d2ed1927ca626ae474b5cea3d1f
k-ta-yamada <key.luvless@gmail.com>
2019-01-29 21:57:18 +0900

    [upd] component/todo-reactive/todo-reactive.component.spec.ts

--- src/app/component/todo-reactive/todo-reactive.component.spec.ts
+++ src/app/component/todo-reactive/todo-reactive.component.spec.ts
@@ -1,16 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoReactiveComponent } from './todo-reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Todo } from 'src/app/models/todo';
 
-describe('TodoReactiveComponent', () => {
+fdescribe('TodoReactiveComponent', () => {
   let component: TodoReactiveComponent;
   let fixture: ComponentFixture<TodoReactiveComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoReactiveComponent ]
+      declarations: [TodoReactiveComponent],
+      imports: [ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +24,39 @@ describe('TodoReactiveComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    expect(component.todoForm.get('task').value).toBe('');
+    expect(component.todoForm.get('done').value).toBeFalsy();
   });
+
+  describe('#add', () => {
+
+    it('task入力時はemitされ、リセットされること', () => {
+      const result = {
+        task: 'new todo',
+        done: true,
+      };
+
+      component.clickAdd.subscribe((r: Todo) => {
+        expect(r).toEqual(result);
+        // expect(r instanceof Todo).toBeTruthy('instanceof Todo');
+      });
+
+      component.todoForm.patchValue(result);
+      component.add();
+
+      expect(component.todoForm.value).toEqual({ task: '', done: false });
+    });
+
+    it('task未入力時はemitされず、リセットもされないこと', () => {
+      spyOn(component.clickAdd, 'emit');
+      spyOn(component.todoForm, 'patchValue');
+
+      component.add();
+
+      expect(component.clickAdd.emit).not.toHaveBeenCalled();
+      expect(component.todoForm.patchValue).not.toHaveBeenCalled();
+    });
+  });
+
 });
